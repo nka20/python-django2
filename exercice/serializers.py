bfaba61e4fc06ec38d83345d1c44ee79bacf763a
nka20 <ntwarilevinkenallan@gmail.com>
2023-08-01 19:43:55 +0200

    modifier ou ajouter

--- exercice/serializers.py
+++ exercice/serializers.py
@@ -8,6 +8,11 @@ class ProduitSerializer(serializers.ModelSerializer):
         model = Produit
         fields = "__all__"
         read_only_fields = 'utilisateur',
+    def to_representation(self,instance):
+        data=super().to_representation(instance)
+        #data['exploit']="ken allan"
+        data['ineza']=instance.nom
+        return data,
 class VenteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vente
