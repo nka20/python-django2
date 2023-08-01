90befebd13733b20c976c3a0f5c5952ac11e5e7a
nka20 <ntwarilevinkenallan@gmail.com>
2023-08-01 19:44:32 +0200

    modifier ou ajouter

--- exercice/serializers.py
+++ exercice/serializers.py
@@ -8,6 +8,8 @@ class ProduitSerializer(serializers.ModelSerializer):
         model = Produit
         fields = "__all__"
         read_only_fields = 'utilisateur',
+
+        #pour ajouter un autre tableau dans le donner qu'on veut
     def to_representation(self,instance):
         data=super().to_representation(instance)
         #data['exploit']="ken allan"
