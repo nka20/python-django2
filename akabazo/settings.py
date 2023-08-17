00c8d2c255dd562a4d61585125854d9777b7f28e
nka20 <ntwarilevinkenallan@gmail.com>
2023-08-17 21:03:05 +0200

    cors

--- akabazo/settings.py
+++ akabazo/settings.py
@@ -50,9 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'akabazo.urls'
+CORS_ORIGIN_ALLOW_ALL=True
 
 TEMPLATES = [
     {
